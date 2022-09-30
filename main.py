b7bdca2609ab0c28cb9e17eb438db1c0c0d0b420
kapilesh <kapilesh1@gmail.com>
2022-10-01 00:22:08 +0530

    basic calculator

--- main.py
+++ main.py
@@ -5,4 +5,21 @@
 
 print('heello world ')
 
+def calculator(num1,num2,operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return abs(num1-num2)
+    elif operator == "*":
+        return num1*num2
+    elif operator == "/":
+        return max(num1,num2)/min(num1,num2)
+    else:
+        return "select a correct operator"
+
+num1 = input('Enter a number1 : ')
+operator = input('Enter a operator +,-,*,/ :')
+num2 = input('Enter a number2 : ')
+
+print(calculator(int(num1),int(num2),operator))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
