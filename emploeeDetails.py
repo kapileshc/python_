a81f92d09624e9036caab4af966df89ae54e78c2
kapilesh <kapilesh1@gmail.com>
2022-10-01 11:12:50 +0530

    Basic OOPS

---
+++ emploeeDetails.py
@@ -0,0 +1,13 @@
+
+
+class EmployeeDetails:
+    def __init__(self,name,age,salary,isavailable):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.isavailable = isavailable
+    def get_name(self):
+        return self.name
+
+
+
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from calculator import calculator
 
 from tdsCalculator import tds_calculator
 
+from emploeeDetails import EmployeeDetails
+
 print('hello world ')
 
 
@@ -12,5 +14,11 @@ print('hello world ')
 # print(int(calculator(int(num1),int(num2),operator)))
 
 print(tds_calculator(500000,False))
+
+emp = EmployeeDetails('kapil',27,20000,True)
+
+
+print(emp.get_name())
+
 
 
